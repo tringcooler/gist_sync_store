c2d4e10d3089c0178793296b51e73181c8855936
tringcooler <tringcooler@users.noreply.github.com>
2018-01-16 21:46:44 +0800

    reformat

--- gist_sync_store.js
+++ gist_sync_store.js
@@ -1,4 +1,4 @@
-var gss = (function () {
+var gss = (function() {
     
     gist_api_url = 'https://api.github.com/gists';
     
@@ -8,7 +8,7 @@ var gss = (function () {
         };
     }
     
-    gist_base.prototype.create = function (suc = null, err = null, timeout = 3, stt_rtry = 0) {
+    gist_base.prototype.create = function(suc = null, err = null, timeout = 3, stt_rtry = 0) {
         $.ajax({
             type: "POST",
             headers: this.headers,
@@ -23,18 +23,18 @@ var gss = (function () {
                 }
             }),
             dataType: "json",
-        }).done(function (data, sta, xhr) {
+        }).done(function(data, sta, xhr) {
             console.log(data);
             var gid = data.id;
             if(suc) suc(gid);
-        }).fail(function (xhr, sta) {
+        }).fail(function(xhr, sta) {
             if(stt_rtry < timeout) return self.create(suc, err, timeout, stt_rtry + 1);
             console.log('err:', sta, xhr.status, xhr.statusText);
             if(err) err(xhr.status);
         });
     };
     
-    gist_base.prototype.edit = function (gid, fdict, suc = null, err = null, timeout = 3, stt_rtry = 0) {
+    gist_base.prototype.edit = function(gid, fdict, suc = null, err = null, timeout = 3, stt_rtry = 0) {
         var self = this;
         var fls = {};
         for(var fn in fdict) {
@@ -53,10 +53,10 @@ var gss = (function () {
                 "files": fls,
             }),
             dataType: "json",
-        }).done(function (data, sta, xhr) {
+        }).done(function(data, sta, xhr) {
             console.log(data);
             if(suc) suc();
-        }).fail(function (xhr, sta) {
+        }).fail(function(xhr, sta) {
             if(stt_rtry < timeout) return self.edit(gid, fdict, suc, err, timeout, stt_rtry + 1);
             console.log('err:', sta, xhr.status, xhr.statusText);
             if(err) err(xhr.status);
@@ -65,7 +65,7 @@ var gss = (function () {
     
     function gist_sync_store(gid = null, sid = null) {
         var ccc = new cc(this);
-        ccc.e(function () {
+        ccc.e(function() {
             var ctx = this;
             ctx.self.gbs = new gist_base();
             if(gid === null) {
@@ -86,13 +86,13 @@ var gss = (function () {
         });
     }
     
-    gist_sync_store.prototype.reload = function () {
+    gist_sync_store.prototype.reload = function() {
         this.store_pool = {};
     };
     
-    gist_sync_store.prototype.test = function () {
+    gist_sync_store.prototype.test = function() {
         var ccc = new cc(this);
-        ccc.e(function () {
+        ccc.e(function() {
             var ctx = this;
             var rf = {}
             for(var i = 0; i < 3; i ++) {
